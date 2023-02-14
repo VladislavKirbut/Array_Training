6b7ab6dadb63ac7391396866de2417e45ce119d2
Vladislav <vladalexkirbut2003@gmail.com>
2023-02-14 23:05:17 +0300

    Added DateTimeUtils class

---
+++ src/DateTimeUtils.java
@@ -0,0 +1,8 @@
+public class DateTimeUtils {
+    public static boolean isLeapYear(int year) {
+        if (year < 0)
+            return false;
+
+        return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+    }
+}
