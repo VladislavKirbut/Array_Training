480ce41f1a4b5b4b17f8ca21189527a05ed55597
Vladislav <vladalexkirbut2003@gmail.com>
2023-02-14 23:44:14 +0300

    Added getIndexOfElement method

--- src/ArrayUtils.java
+++ src/ArrayUtils.java
@@ -1,5 +1,3 @@
-import java.util.Arrays;
-
 public class ArrayUtils {
 
     public static double getAverageNumber(int[] array) {
@@ -28,4 +26,16 @@ public class ArrayUtils {
 
         return newArr;
     }
-}
+
+    public static int getIndexOfElement(int[] array, int element) {
+        if (array == null || array.length == 0)
+            return -1;
+
+        for (int i = 0; i < array.length; i++)
+            if (array[i] == element)
+                return i;
+
+        return -1;
+    }
+  }
+
