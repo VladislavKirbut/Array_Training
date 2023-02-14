ae1688c01529c7520d397ee01c99d35f3ab91cb8
Vladislav <vladalexkirbut2003@gmail.com>
2023-02-14 23:33:57 +0300

    Added reverseArray function and corrected getAverageNumber function

--- src/ArrayUtils.java
+++ src/ArrayUtils.java
@@ -1,12 +1,31 @@
+import java.util.Arrays;
+
 public class ArrayUtils {
+
     public static double getAverageNumber(int[] array) {
+        if (array == null || array.length == 0)
+            return 0;
+
         double sum = 0;
-        int count = 0;
+        int count = 0; // counter
         for (int i = 0; i < array.length; i++) {
             sum += array[i];
             count++;
         }
 
         return sum / count;
+    }
+
+    public static int[] reverseArray(int[] array) {
+        if (array == null || array.length == 0)
+            throw new IllegalArgumentException("Enter a correct array.");
+
+        int[] newArr = new int[array.length];
+        int count = 0;
+
+        for (int i = array.length - 1; i >= 0; i--)
+            newArr[count++] = array[i];
+
+        return newArr;
     }
 }
