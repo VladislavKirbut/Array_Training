75395634ebe9c66442f5ddc6c5efe7fdeaaab73b
Vladislav <vladalexkirbut2003@gmail.com>
2023-02-15 00:33:37 +0300

    Added UserInputUtil class and realyze createArray method

---
+++ src/UserInputUtils.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class UserInputUtils {
+    public static int[] createArray() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter length of array: ");
+        int lengthOfArray = scanner.nextInt();
+        if (lengthOfArray < 0)
+            throw new IllegalArgumentException("Enter correct data.");
+
+        int[] newArr = new int[lengthOfArray];
+        System.out.println("Enter elements of array: ");
+
+        for (int i = 0; i < newArr.length; i++) {
+            System.out.print("Element number " + (i + 1) + ": ");
+            newArr[i] = scanner.nextInt();
+        }
+
+        return newArr;
+    }
+}
